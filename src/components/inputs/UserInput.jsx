c607b7cea42df4df4c1741b4993fb81abd8fd374
Misa24jr <misa24jr@gmail.com>
2024-04-08 19:46:49 -0400

    Alineado de nickname y password de usersettings

--- src/components/inputs/UserInput.jsx
+++ src/components/inputs/UserInput.jsx
@@ -4,27 +4,32 @@ import { StyleSheet, View, TextInput, Text } from "react-native";
 const UserInput = ({ placeholder, name, value, secureTextEntry, editable = true }) => {
     return (
         <View style={styles.containerInput}>
-            <Text style={styles.name}>{name}</Text>
-            <TextInput
-                style={styles.input}
-                placeholder={placeholder}
-                placeholderTextColor={'#ffffff'}
-                value={value}
-                secureTextEntry={secureTextEntry}
-                editable={editable} // Aquí se configura la propiedad editable del TextInput
-                onChangeText={(text) => console.log(text)} // Puedes agregar tu lógica de manejo de cambios aquí
-            />
+            <View style={styles.containerNickname}>
+                <Text style={styles.name}>{name}</Text>
+            </View>
+            
+            <View style={styles.containerInputText}>
+                <TextInput
+                    style={styles.input}
+                    placeholder={placeholder}
+                    placeholderTextColor={'#ffffff'}
+                    value={value}
+                    secureTextEntry={secureTextEntry}
+                    editable={editable} // Aquí se configura la propiedad editable del TextInput
+                    onChangeText={(text) => console.log(text)} // Puedes agregar tu lógica de manejo de cambios aquí
+                />
+            </View>
+
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     containerInput: {
-        width: '100%',
-        gap: 20,
+        width: 280,
+        gap: 25,
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'center',
         alignItems: 'center',
     },
     input: {
@@ -37,9 +42,15 @@ const styles = StyleSheet.create({
     },
     name: {
         color: '#8CCECC',
-        fontSize: 12,
+        fontSize: 15,
         fontFamily: 'Jura_400Regular',
         fontWeight: 'bold',
+    },
+    containerNickname:{
+        width: "25%",
+    },
+    containerInputText:{
+        width: "70%",
     }
 });
 
