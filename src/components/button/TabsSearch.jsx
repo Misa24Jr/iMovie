6b2ec6307f7bd6a5e664dea1510f2026475bb1b5
Misa24jr <misa24jr@gmail.com>
2024-04-09 08:12:15 -0400

    Componente TabsSearch ya filtra segun el boton que se toca

--- src/components/button/TabsSearch.jsx
+++ src/components/button/TabsSearch.jsx
@@ -1,24 +1,27 @@
-import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const TabsSearch = ({ tabs, activeTab, setActiveTab }) => {
+const TabsSearch = ({ setActiveTab }) => {
+    const [activeButton, setActiveButton] = useState('All');
+
     return(
         <View style={style.container}>
-            <TouchableOpacity>
-                <Text style={style.textt}>All</Text>
+            <TouchableOpacity onPress={() => { setActiveTab('All'); setActiveButton('All'); }}>
+                <Text style={[style.text, activeButton === 'All' && style.activeText]}>All</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-                <Text style={style.text}>Movie</Text>
+            <TouchableOpacity onPress={() => { setActiveTab('Movies'); setActiveButton('Movies'); }}>
+                <Text style={[style.text, activeButton === 'Movies' && style.activeText]}>Movies</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-                <Text style={style.text}>Tv Shows</Text>
+            <TouchableOpacity onPress={() => { setActiveTab('Tv Shows'); setActiveButton('Tv Shows'); }}>
+                <Text style={[style.text, activeButton === 'Tv Shows' && style.activeText]}>Tv Shows</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-                <Text style={style.text}>Most Recent</Text>
+            <TouchableOpacity onPress={() => { setActiveTab('Most Recent'); setActiveButton('Most Recent'); }}>
+                <Text style={[style.text, activeButton === 'Most Recent' && style.activeText]}>Most Recent</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-                <Text style={style.text}>Most Rated</Text>
+            <TouchableOpacity onPress={() => { setActiveTab('Most Rated'); setActiveButton('Most Rated'); }}>
+                <Text style={[style.text, activeButton === 'Most Rated' && style.activeText]}>Most Rated</Text>
             </TouchableOpacity>
+            {/* Agrega más botones según sea necesario */}
         </View>
     )
 };
@@ -32,22 +35,18 @@ const style = StyleSheet.create({
         alignItems: 'center',
         width: 370,
         height: 40,
-        paddingLeft: 5,
-        paddingRight: 5,
+        padding: 5,
     },
     text:{
         color: '#fff',
-        fontSize: 16,
+        fontSize: 14,
         fontFamily: 'Jura_400Regular',
         padding: 5,
+        fontWeight: 'bold',
     },
-    textt:{
-        color: '#3C5252',
-        fontSize: 16,
-        fontFamily: 'Jura_400Regular',
-        padding: 5,
-        fontWeight: "900",
-    },
+    activeText: {
+        color: '#3C5252', // Color de fondo activo
+    }
 });
 
 export default TabsSearch;
--- src/views/SearchView.jsx
+++ src/views/SearchView.jsx
@@ -1,16 +1,16 @@
-import React, {useState} from "react";
-import { StyleSheet, ScrollView, Text, Image, TouchableOpacity, View, TextInput} from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, Text, Image, TouchableOpacity, View, TextInput} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-
 // Components
-import AccordionSearch from "../components/others/AccordionSearch";
-import AccordionGenre from "../components/others/AccordionGenre";
-import TabsSearch from "../components/button/TabsSearch";
-import BoxMovie from "../components/containers/BoxMovie";
+import AccordionSearch from '../components/others/AccordionSearch';
+import AccordionGenre from '../components/others/AccordionGenre';
+import TabsSearch from '../components/button/TabsSearch';
+import BoxMovie from '../components/containers/BoxMovie';
 
 const SearchView = () => {
     const navigation = useNavigation();
+    const [activeTab, setActiveTab] = useState('All');
 
     return(
         <>
@@ -21,7 +21,7 @@ const SearchView = () => {
                 <TouchableOpacity
                     onPress={() => navigation.navigate('NewsView')}
                     style={{marginBottom: 20}}
-                    >
+                >
                     <Image
                         style={{width: 30, height: 30}}
                         source={require('../../assets/back.png')}
@@ -31,18 +31,60 @@ const SearchView = () => {
                 <View style={style.containerSearch}>
                     <AccordionSearch title={'Search'}/>
                     <AccordionGenre title={'Filters'}/>
-                    <TabsSearch/>
-                    <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
-                    <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
-                    <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
-                    <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
-                    <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
-                    <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
-                    <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
-                    <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
+                    <TabsSearch setActiveTab={setActiveTab}/>
+                    {/* Renderizar los resultados según la pestaña activa */}
+                    {activeTab === 'All' && (
+                        <>
+                            <View style={style.containerOption}>
+                                <Text style={style.textOption}>All content</Text>
+                                <BoxMovie title={'The Shawshank Redemption'} year={1994} autors={'Frank Darabont'} rating={'91%'}/>
+                                <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
+                                <BoxMovie title={'Matrix'} year={1999} autors={'The Wachowskis'} rating={'87%'}/>
+                                <BoxMovie title={'The Lord of the Rings: The Fellowship of the Ring'} year={2001} autors={''} rating={'91%'}/>
+                                <BoxMovie title={'The Godfather'} year={1972} autors={'Francis Ford Coppola'} rating={'98%'}/>
+                            </View>
+                        </>
+                    )}
+                    {activeTab === 'Movies' && (
+                        <>
+                            <View style={style.containerOption}>
+                                <Text style={style.textOption}>Movie content</Text>
+                                <BoxMovie title={'The Shawshank Redemption'} year={1994} autors={'Frank Darabont'} rating={'91%'}/>
+                                <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
+                            </View>
+                        </>
+                    )}
+                    {activeTab === 'Tv Shows' && (
+                        <>
+                            <View style={style.containerOption}>
+                                <Text style={style.textOption}>TV show content</Text>
+                                <BoxMovie title={'Breaking Bad'} year={2008} autors={'Vince Gilligan'} rating={'96%'}/>
+                                <BoxMovie title={'Game of Thrones'} year={2011} autors={'David Benioff - D. B. Weiss'} rating={'89%'}/>
+                            </View>
+                        </>
+                    )}
+
+                    {activeTab === 'Most Recent' && (
+                        <>
+                            <View style={style.containerOption}>
+                                <Text style={style.textOption}>Most recent content</Text>
+                                <BoxMovie title={'Avengers: ENDGAME'} year={2012} autors={'Misael Reverol - Isabella Fonseca'} rating={'64%'}/>
+                                <BoxMovie title={'The Lord of the Rings: The Fellowship of the Ring'} year={2001} autors={''} rating={'91%'}/>
+                            </View>
+                        </>
+                    )}
+
+                    {activeTab === 'Most Rated' && (
+                        <>
+                            <View style={style.containerOption}>
+                                <Text style={style.textOption}>Most rated content</Text>
+                                <BoxMovie title={'The Godfather'} year={1972} autors={'Francis Ford Coppola'} rating={'98%'}/>
+                                <BoxMovie title={'The Shawshank Redemption'} year={1994} autors={'Frank Darabont'} rating={'91%'}/>
+                            </View>
+                        </>
+                    )}
+                    
                 </View>
-
-
             </ScrollView>
         </>
     )
@@ -64,6 +106,15 @@ const style = StyleSheet.create({
         fontSize: 16,
         fontFamily: 'Jura_400Regular',
     },
+    textOption:{
+        color: '#fff',
+        fontSize: 16,
+        fontFamily: 'Jura_400Regular',
+        marginTop: 20,
+    },
+    containerOption:{
+
+    }
 });
 
 export default SearchView;
